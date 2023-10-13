82adda747079faad4fb1d468d900b4b088777e88
Yeaeun411 <yeaeun411@naver.com>
2023-10-14 00:26:19 +0900

    add Likes

--- src/main/java/com/example/demo/post/like/LikeRepository.java
+++ src/main/java/com/example/demo/post/like/LikeRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
 
+    void deleteByUserIdAndPostId(Long userId, Long postId);
 }
